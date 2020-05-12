627457ca60362b77900645db7a8d994b2a9f1b05
Habiba-Naeem <habiba.abdulnaeem@gmail.com>
2020-05-13 02:03:29 +0500

    registration and login done, lacks a few good front end working but okay. Messages also added

--- orders/static/orders/mainscript.js
+++ orders/static/orders/mainscript.js
@@ -1,8 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var topping_type_option = document.querySelector(".pizza-select");
-    var topping = document.querySelector(".pizza-select-toppings");
-    
-    if (topping_type_option.options[0].value === 'Cheese'){
-        topping.disabled = "true";
+function linking(link, id){
+    if(window.location.href !== "http://localhost:8000/"){
+            link.href = `http://localhost:8000/${id}`;
     }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    var menu_section = document.querySelector("#menu");
+    var menu_link = document.querySelector("#menu-link");
+    var home_link = document.querySelector("#home-link");
+
+    var login_article = document.querySelector("#login");
+    var register_article = document.querySelector("#register");
+
+    var top_reg_button = document.querySelector("#top-reg-button");
+    var top_log_button = document.querySelector("#top-log-button");
+
+    var login_button = document.querySelector("#login-button");
+    var register_button = document.querySelector("#register-button");
+    menu_link.addEventListener("click", ()=>{
+        linking(menu_link, "#menu");
+    })
+
+    home_link.addEventListener("click", ()=>{
+        linking(home_link, "#heading");
+    })
+
+    top_log_button.addEventListener("click", ()=>{
+        if(login_article.style.display === "none"){
+            login_article.style.display = "block";
+            register_article.style.display = "none";
+        }
+        else{
+            login_article.style.display = "none";
+            register_article.style.display = "block";
+        }
+    })
+
+    top_reg_button.addEventListener("click", ()=>{
+        if(register_article.style.display === "none"){
+            register_article.style.display = "block";
+            login_article.style.display = "none";
+        }
+        else{
+            register_article.style.display = "none";
+            login_article.style.display = "block"
+        }
+    })
 })
--- user/views.py
+++ user/views.py
@@ -5,15 +5,17 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 #from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db import IntegrityError
 
 # Create your views here
 def index(request):
     if not request.user.is_authenticated:
+        print("okay")
         return render(request, "user/index.html", {"message": None})
     context = {
         "user": request.user
     }
-    return render(request, "user/index.html", context)
+    return render(request, "orders/cart.html", context)
 
 def login_user(request):
     username = request.POST.get("username")
@@ -22,14 +24,15 @@ def login_user(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
+        print("loogedin")
         return HttpResponseRedirect(reverse("index"))
     else:
-        return render(request, "user/index.html", {"message": "Please provide correct username and password."})
-
-
+        messages.warning(request, "Please provide correct username and password.")
+        return HttpResponseRedirect(reverse("index"))
+        
 def logout_user(request):
     logout(request)
-    return render(request, "user/index.html", {"message": "Logged out."})
+    return HttpResponseRedirect(reverse("index"))
 
 def register(request):
 
@@ -42,25 +45,25 @@ def register(request):
         password = request.POST.get('password')
         confirm_password = request.POST.get('confirm-password')
 
-        
-        check_user = authenticate( username = username, email = email)
-
-        if check_user is None:
-
-            if password == confirm_password:
-            
-                user = User.objects.create_user( username = username, first_name = first_name, last_name = last_name, email = email, password = password)
-                messages.success(request, f'Your account has been created! You are now able to log in')
-                return redirect('login')
-            
-            else:
-                return render(request, 'user/index.html', {'message': "Provide matching password"})
-        
+        if password == confirm_password:
+            try:
+                if User.objects.get(username=username) or User.objects.get(email=email):
+                    print("unscunceess")
+                    messages.error(request, "Username or email already exists.")
+                    return HttpResponseRedirect(reverse("register"))
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password)
+                messages.success(request, "Account created")
+                user_log = authenticate(request, username=username, password=password)
+                login(request, user_log)
+                return HttpResponseRedirect(reverse('index'))
         else:
-            return render(request, 'user/index.html', {'message': "User already exists"})
-
+            messages.error(request, "Passwords do not match")
+            return HttpResponseRedirect(reverse("register"))
+           
     return render(request, 'user/index.html')
 
+       
 """
 @login_required
 def profile(request):
